9adf197920fbc02c1adfa68fe6bc57e46dadff87
aidanmuller <70919159+aidanmuller@users.noreply.github.com>
2020-09-08 18:08:47 +0100

    Update irishcensus.py
    Added getcensusresults()

--- python2/irishcensus.py
+++ python2/irishcensus.py
@@ -1,29 +1,32 @@
 # http://www.census.nationalarchives.ie/search/results.jsp?searchMoreVisible=false&census_year=1911&surname=Muller&firstname=&county19011911=&county1821=&county1831=&county1841=&county1851=&barony=&parish=&ward=&townland=&houseNumber=&familyId=&ded=&age=&sex=&search=Search&ageInMonths=&relationToHead=&religion=&education=&occupation=&marriageStatus=&yearsMarried=&birthplace=&nativeCountry=&language=&deafdumb=&causeOfDeath=&yearOfDeath=&familiesNumber=&malesNumber=&femalesNumber=&maleServNumber=&femaleServNumber=&estChurchNumber=&romanCatNumber=&presbNumber=&protNumber=&marriageYears=&childrenBorn=&childrenLiving=
-#nd = d[d.find('Displaying resul')-1:len(d)]
-#npages = int(nd[nd.find('Displaying results 1 - 10 of ')+29:nd.find('</span>')])
 
 from bs4 import BeautifulSoup
 import urllib2 as u
 import re
 
-def getcensus1911(forename='', surname='', county='', town='', result=0):
-    # Get URL of census query
+def getcensusresults(forename='', surname='', county='', town=''):
+    # GET URL TO QUERY
+    url = 'http://www.census.nationalarchives.ie/search/results.jsp?searchMoreVisible=false&census_year=1911&surname={}&firstname={}&county19011911={}&county1821=&county1831=&county1841=&county1851=&barony=&parish=&ward=&townland={}&houseNumber=&familyId=&ded=&age=&sex=&search=Search&ageInMonths=&relationToHead=&religion=&education=&occupation=&marriageStatus=&yearsMarried=&birthplace=&nativeCountry=&language=&deafdumb=&causeOfDeath=&yearOfDeath=&familiesNumber=&malesNumber=&femalesNumber=&maleServNumber=&femaleServNumber=&estChurchNumber=&romanCatNumber=&presbNumber=&protNumber=&marriageYears=&childrenBorn=&childrenLiving='.format(surname, forename, county, town)
+    d = u.urlopen(url).read()
+    # FIND STRING IN HTML CONTAINING NUMBER OF RESULTS FROM SEARCH
+    results = d[d.find('<span class="short">'):len(d)]
+    results = results[0:results.find('</span>')+7]
+    # NARROW THE STRING FROM 'DISPLAYING RESULTS X-Y of Z' to just 'Z'
+    results = results[results.find('of ')+3:len(results)-7]
+    return int(results)
+
+def getcensus(forename='', surname='', county='', town='', result=0):
+    # GET URL TO QUERY
     url = 'http://www.census.nationalarchives.ie/search/results.jsp?searchMoreVisible=false&census_year=1911&surname={}&firstname={}&county19011911={}&county1821=&county1831=&county1841=&county1851=&barony=&parish=&ward=&townland={}&houseNumber=&familyId=&ded=&age=&sex=&search=Search&ageInMonths=&relationToHead=&religion=&education=&occupation=&marriageStatus=&yearsMarried=&birthplace=&nativeCountry=&language=&deafdumb=&causeOfDeath=&yearOfDeath=&familiesNumber=&malesNumber=&femalesNumber=&maleServNumber=&femaleServNumber=&estChurchNumber=&romanCatNumber=&presbNumber=&protNumber=&marriageYears=&childrenBorn=&childrenLiving=&pager.offset={}'.format(surname, forename, county, town, result)
     d = u.urlopen(url).read()
-    # Find where last name is stored in HTML
-    # All Census information is in a tr class called odd
-    # Find odd in result of query and locate the link which has the person's last name in it
+    # FIND LAST NAME AND FIRST NAME IN SEARCH RESULT
     lname = d[d.find('"odd"')+6:len(d)]
     tdpoint = lname.find('</td>')
     lname = lname[4:lname.find('</td>')]
-    # Parse <a href> and find what the contents of the <a href> is (the lastname)
     lname = BeautifulSoup(lname, features="lxml").a.string
-    # Find odd
     fname = d[d.find('"odd"')+6:len(d)]
-    # The first name is stored right after the last so we can just search the tags after the end of the <a href>
     fname = fname[tdpoint+5:len(d)]
     fname = fname[4:fname.find('</td')]
-    # Parse for first name
     fname = BeautifulSoup(fname, features="lxml").a.string
     return str(lname), str(fname)
 
